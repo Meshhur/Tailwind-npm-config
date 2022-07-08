1e34b70bfc9f891a530b02d5706c1a6b5762388f
Meshhur <meshhuratageldiyev010@gmail.com>
2022-07-08 21:04:33 +0500

    added packeges

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ['./src/*.html'],
+    theme: {
+        extend: {
+            colors: {
+                'super-blue-color': "#0e8ef1",
+            },
+        },
+    },
+    variants: {
+        extend: {
+
+        },
+    },
+    plugins: [],
+};
